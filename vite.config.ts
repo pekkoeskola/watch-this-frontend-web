aa58717a9e03feaf39844255ddf259186ade42f0
pekkoeskola <pekko.eskola@hotmail.fi>
2025-02-27 18:49:37 +0200

    Made necessary modifications to enable shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   }
 });
